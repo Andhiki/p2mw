71f5da444f5ee7ccfa3a26921f7c279797fe680b
Andre <andhiki28@gmail.com>
2025-09-27 21:37:35 +0700

    fix (fetch): jujur astro aura

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,34 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-	},
+    plugins: [tailwindcss()],
+  },
 
   integrations: [react()],
+
+  // Enable content collections
+  content: {
+    collections: {
+      course: {
+        type: "content",
+        schema: {
+          type: "object",
+          properties: {
+            layout: { type: "string" },
+            title: { type: "string" },
+            slug: { type: "string" },
+            thumbnail: { type: "string" },
+          },
+          required: ["title", "slug"],
+        },
+      },
+    },
+  },
 });
